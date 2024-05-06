6fc5b14dd1749c538463a74f68a16e6ac56049f9
jiangjie <509745383@qq.com>
2024-05-06 10:55:45 +0800

    finish pgtbl task2 vmprint

--- kernel/vm.c
+++ kernel/vm.c
@@ -449,3 +449,32 @@ copyinstr(pagetable_t pagetable, char *dst, uint64 srcva, uint64 max)
     return -1;
   }
 }
+
+
+void
+vmprint(pagetable_t pagetable){
+    static int deepth = 0;
+    if(deepth == 0){
+        printf("page table %p\n",pagetable);
+    }
+    for(int i=0;i<512;i++){
+        pte_t pte = pagetable[i];
+        if((pte & PTE_V) && (pte & (PTE_R|PTE_W|PTE_X)) == 0){
+            // this PTE points to a lower-level page table.
+            for(int j=0;j<=deepth;j++){
+                printf(" ..");
+            }
+            uint64 child = PTE2PA(pte);
+            printf("%d: pte %p pa %p\n",i,pte,child);
+            deepth ++;
+            vmprint((pagetable_t)child);
+            deepth -- ;
+        } else if(pte & PTE_V){
+            for(int j=0;j<=deepth;j++){
+                printf(" ..");
+            }
+            uint64 child = PTE2PA(pte);
+            printf("%d: pte %p pa %p\n",i,pte,child);
+        }   
+    }
+}
